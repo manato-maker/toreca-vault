af9900bb5db721b80c4468ac2c8cfd36a0592838
manato-maker <manato.pt@gmail.com>
2026-09-17 22:24:00 +0900

    Force clients onto fixed inventory storage version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='toreca-vault-v11';
+const CACHE='toreca-vault-v12';
 const ASSETS=['./','./index.html','./styles.css','./js/app.js','./js/remote-sync.js','./js/schema.js','./js/storage.js','./js/calculations.js','./js/inventory.js','./market-update.json','./manifest.webmanifest','./assets/icon.svg'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))})
