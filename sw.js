91653c40765ef576836f4569efd75bad25b35f92
manato-maker <manato.pt@gmail.com>
2026-09-23 02:47:14 +0900

    Bump service worker cache to v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='toreca-vault-v14';
+const CACHE='toreca-vault-v15';
 const ASSETS=['./','./index.html','./styles.css','./js/app.js','./js/presentation.js','./js/ui-enhancements.js','./js/franchise.js','./js/franchise-ui.js','./js/remote-sync.js','./js/schema.js','./js/storage.js','./js/calculations.js','./js/inventory.js','./market-update.json','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
